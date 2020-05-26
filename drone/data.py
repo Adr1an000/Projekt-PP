19baaf25321473e773b71159d8f4b5f7a124d78e
Adr1an000 <32631648+Adr1an000@users.noreply.github.com>
2020-05-26 08:56:52 +0200

    fft

--- drone/data.py
+++ drone/data.py
@@ -1,7 +1,9 @@
 from mat73 import loadmat
 from functools import reduce
 from scipy.fft import fft
+from scipy.fftpack import fftfreq
 import numpy as np
+
 def mapRecordToCas(record):
     casRecord = [0, 0, 0, 0, 0]
     avrg = record[5:21].mean()
@@ -13,40 +15,48 @@ def mapRecordToCas(record):
 
     return casRecord
 
+class fftData:
+    def __init__(self, freq, firstColumnFFT, secondColumnFFT, frequencyPresent):
+        self.freq = freq
+        self.firstColumnFFT = firstColumnFFT
+        self.secondColumnFFT = secondColumnFFT
+        self.frequencyPresent = frequencyPresent
 
 def casPieceToFreq(casPiece):
     firstColumn = list(map(lambda c: c[0], casPiece))
     secondColumn = list(map(lambda c: c[1], casPiece))
 
-    freq = fft.freq(len(firstColumn))
+    freq = fftfreq(len(firstColumn))
     firstColumnFFT = fft(firstColumn)
     secondColumnFFT = fft(secondColumn)
 
-    isFrequencyPresent = reduce(lambda a, c: a + c[4], casPiece)
+    frequencyPresent = reduce(lambda a, c: a + c[4], casPiece, 0)
 
-    return freq, firstColumnFFT, secondColumnFFT, isFrequencyPresent
-
-
-def filterCasPiecesZero(casPice):
-    return reduce(lambda a, r: a + r[4], casPice) == 0
+    return fftData(freq, firstColumnFFT, secondColumnFFT, frequencyPresent)
 
 if __name__ == "__main__":
     mat = loadmat('dane.mat')
 
     cas = list(map(mapRecordToCas, mat['matrix']))
-    print(type(cas))
-    print(type(cas[0]))
-    print(cas[0:10])
 
     i = 2000
     step = 2000
+    averageBackgroundFrequency = fftData(fftfreq(step), np.zeros(step), np.zeros(step), False)
 
-    casPieces = []
-    while i < len(cas):
-        casPieces.append(cas[i-2000:i])
+    while i < 2001:
+        piece = cas[i-2000:i]
+        
+        frequencies = casPieceToFreq(piece)
+        if not frequencies.frequencyPresent:
+            averageBackgroundFrequency.firstColumnFFT = np.add(averageBackgroundFrequency.firstColumnFFT, frequencies.firstColumnFFT)
+            averageBackgroundFrequency.secondColumnFFT = np.add(averageBackgroundFrequency.secondColumnFFT, frequencies.secondColumnFFT)
         i += 1
+    averageBackgroundFrequency.firstColumnFFT = [abs(x) / step for x in averageBackgroundFrequency.firstColumnFFT]
+    averageBackgroundFrequency.secondColumnFFT = [abs(x) / step for x in averageBackgroundFrequency.secondColumnFFT]
+    #firstmax = max(averageBackgroundFrequency.firstColumnFFT[1:])
+    #print(firstmax)
+    #for f in averageBackgroundFrequency.firstColumnFFT[1:]:
+    #    print ("*" * int(f / firstmax * 100))
 
-    map
 
-    zeroPieces = list(filter(filterCasPiecesZero, casPieces))
 
